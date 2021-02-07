7ed2649fcfc70d045f106a40a4124f65337a3a37
Şaban Kayış <sbnkys1@gmail.com>
2021-02-07 21:31:05 +0300

    storing data on local database

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,7 +1,7 @@
 import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACES = "ADD_PLACES";
-
+import { insertPlace } from "../helpers/db";
 export const addPlaces = (title,image) => {
   return async dispatch =>{
     const fileName = image.split("/").pop();
@@ -12,12 +12,14 @@ export const addPlaces = (title,image) => {
         from:image,
         to:newPath
       });
+      const dbResult = await insertPlace(title,newPath,'Dummy Address',15.6,12.3);
+      console.log(dbResult);
+    dispatch({ type: ADD_PLACES, placeData: { id: dbResult.insertId , title: title , image:newPath } });
     }
     catch(err){
       console.log(err);
       throw err;
     }
 
-    dispatch({ type: ADD_PLACES, placeData: { title: title , image:newPath } });
   }
 };
