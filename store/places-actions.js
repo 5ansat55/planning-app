540e43d9c6354e12d27e67b729c78e12ace5af04
Şaban Kayış <sbnkys1@gmail.com>
2021-02-07 14:07:59 +0300

    stroing the image fileSystem

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,24 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACES = "ADD_PLACES";
 
 export const addPlaces = (title,image) => {
-  return { type: ADD_PLACES, placeData: { title: title , image:image } };
+  console.log(image);
+  return async dispatch =>{
+    const fileName = image.split("/")
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from:image,
+        to:newPath
+      });
+    }
+    catch(err){
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACES, placeData: { title: title , image:newPath } });
+  }
 };
